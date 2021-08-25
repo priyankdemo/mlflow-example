130a226152195e3c1c88da611ac126f5d29b69a8
priyankdemo <85923718+priyankdemo@users.noreply.github.com>
2021-08-25 10:06:48 -0500

    Add files via upload

---
+++ repo-info.py
@@ -0,0 +1,19 @@
+# importing the requests library
+import requests
+
+# defining the api-endpoint
+API_ENDPOINT = "http://5802-115-119-250-30.ngrok.io/train"
+
+# data to be sent to api
+data = {
+	"url": "https://github.com/priyankdemo/mlflow-example",
+	"branch_name": "master",
+	"algo_type": "Regression"
+}
+
+# sending post request and saving response as response object
+r = requests.post(url = API_ENDPOINT, data = data)
+
+# extracting response text
+pastebin_url = r.text
+print("The pastebin URL is:%s"%pastebin_url)
